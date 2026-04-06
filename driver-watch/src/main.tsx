86449da09429cd8ca95ecac1adb0c1a6100014b9
unknown <tanayprayagkinariwala@gmail.com>
2026-04-06 16:59:24 +0530

    try2

--- driver-watch/src/main.tsx
+++ driver-watch/src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const redirectPath = new URLSearchParams(window.location.search).get("path");
+
+if (redirectPath) {
+	window.history.replaceState(null, "", decodeURIComponent(redirectPath));
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
